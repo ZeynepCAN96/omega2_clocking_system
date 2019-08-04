33eed880f954129053542d1cfb56e812ff63cf65
Francisco Olivares <folivares@arenasoft.cl>
2019-08-04 18:49:35 +0100

    Testing image path

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ from enum import Enum
 class Config(Enum):
     DELAY_MINUTES = 10 #DELAY FOR CLOCKING AGAIN IN SECONDS (AVOID DUPLICATE)
     OLED_EXPANSION = 1 #1 = ON OR 0 = OFF (USE OLED EXPANSION)
-    OK_IMG = "/root/omega2_clocking_system/img/image_ok.lcd" #Path for OK img
-    ERROR_IMG = "/root/omega2_clocking_system/img/image_error.lcd" #Path for error img
+    OK_IMG = "img/image_ok.lcd" #Path for OK img
+    ERROR_IMG = "img/image_error.lcd" #Path for error img

c9cddb4eb95ff775ca12f020194c53c5aa6f95e3
Panchop <folivares@arenasoft.cl>
2019-08-07 13:06:42 +0100

    Delete comment in main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ If a tag is found the uid is compared with a database
 and the data is sent to a google spreadsheet
 """
 
-#Python Modules
-
 #User Modules
 from card import Card
 from database import Database

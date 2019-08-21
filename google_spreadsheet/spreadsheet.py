b98587bf0d1e0e9535913a9fb866203ec7caba84
Panchop <folivares@arenasoft.cl>
2019-08-21 13:03:15 +0100

    change google key filename

--- google_spreadsheet/spreadsheet.py
+++ google_spreadsheet/spreadsheet.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pprint
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('omega_ned.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('omega_ned_key.json', scope)
 client = gspread.authorize(creds)
 
 if __name__ == '__main__':
